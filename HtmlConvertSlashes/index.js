0ec3645c4430c2f758174959ffe2a31dc785f2ec
toddnguyen47 <todd.nguyen47@gmail.com>
2018-11-15 16:03:21 -0800

    Adding "\" to "/" conversion

---
+++ HtmlConvertSlashes/index.js
@@ -0,0 +1,23 @@
+console.log("Yo");
+
+var submitButton = document.getElementById("submitButton");
+submitButton.onclick = function() {
+	let initTextElem = document.getElementById("initInput");
+	let contents = initTextElem.value;
+
+	let replaced = String(contents).replace(/\\/g, "/");
+	let outputElem = document.getElementById("textOutput");
+	outputElem.value = replaced;
+	// Auto select and focus
+	outputElem.focus();
+	outputElem.select();
+}
+
+
+var copyButton = document.getElementById("copyButton");
+copyButton.onclick = function() {
+	document.execCommand('copy');
+}
+
+
+console.log("End");
