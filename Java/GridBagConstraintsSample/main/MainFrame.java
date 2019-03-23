51fdb746738cd1e5d80314edd4d9d0d7fde5fa9f
Todd Nguyen <todd.nguyen47@gmail.com>
2019-03-23 14:21:57 -0700

    Added SwingWorker examples

--- Java/GridBagConstraintsSample/main/MainFrame.java
+++ Java/GridBagConstraintsSample/main/MainFrame.java
@@ -1,38 +1,36 @@
 package main;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.Font;
-import java.util.Enumeration;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.FontUIResource;
+import javax.swing.BorderFactory;
 
 @SuppressWarnings("serial")
-public class MainFrame extends JFrame {
-    public MainFrame() {
+public class MainFrame extends javax.swing.JFrame {
+    public MainFrame(JPanel mainPanel) {
+        this.initComponents(mainPanel);
+    }
+    
+    
+    public void initComponents(JPanel mainPanel) {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Test");
         
-        JPanel contentPane = new JPanel();
+        JPanel contentPane2 = new JPanel();
         // Set Border
-        contentPane.setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2), 
+        contentPane2.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(java.awt.Color.LIGHT_GRAY, 2), 
                 BorderFactory.createEmptyBorder(5, 5, 5, 5)));
         
         // Set Layout
-        // contentPane.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));        
-        contentPane.setLayout(new java.awt.GridBagLayout());
-        java.awt.GridBagConstraints gbc = new java.awt.GridBagConstraints();
-        gbc.weightx = gbc.weighty = 1.0;
-        gbc.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gbc.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        contentPane2.setLayout(new BorderLayout());
         
-        MainPanel mainPanel = new MainPanel();
-        contentPane.add(mainPanel, gbc);
+        // Add the main panel to this content pane 2
+        contentPane2.add(mainPanel, BorderLayout.CENTER);
 
         
         // +-----------------------------------+
@@ -40,7 +38,7 @@ public class MainFrame extends JFrame {
         // +-----------------------------------+
         
         // Make a ScrollPane
-        javax.swing.JScrollPane jsp = new javax.swing.JScrollPane(contentPane);
+        javax.swing.JScrollPane jsp = new javax.swing.JScrollPane(contentPane2);
         // Always show the scroll bar
         jsp.setVerticalScrollBarPolicy(javax.swing.JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         // Increase the mouse wheel scroll speed
@@ -58,20 +56,24 @@ public class MainFrame extends JFrame {
     // Set font for all components
     // Reference: https://stackoverflow.com/a/12730398
     public static void setUIFont(FontUIResource f) {
-        Enumeration<Object> keys = UIManager.getDefaults().keys();
+        java.util.Enumeration<Object> keys = UIManager.getDefaults().keys();
         while (keys.hasMoreElements()) {
             Object key = keys.nextElement();
             Object value = UIManager.get(key);
             if (value instanceof FontUIResource) {
                 FontUIResource orig = (FontUIResource) value;
-                Font font = new Font(f.getFontName(), orig.getStyle(), f.getSize());
+                Font font = new Font(f.getFontName(),
+                        orig.getStyle(), f.getSize());
                 UIManager.put(key, new FontUIResource(font));
             }
         }
     }
     
     
-    public static void main(String[] args) {
+    /**
+     * This function needs to be run BEFORE we make any components!
+     */
+    public static void setFontsAndLookAndFeel() {
         // FIRST STEP: Set look and feel
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -86,7 +88,22 @@ public class MainFrame extends JFrame {
         }
         
         // Set font size
-        MainFrame.setUIFont(new FontUIResource(new Font("Arial", Font.PLAIN, 16)));
-        new MainFrame();
+        MainFrame.setUIFont(new FontUIResource(new Font(Font.SANS_SERIF, Font.PLAIN, 16)));
+    }
+    
+    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                // This function needs to be run BEFORE we make any components!
+                MainFrame.setFontsAndLookAndFeel();
+                
+                // Now that we have set a look and feel, we can initialize components
+                MainPanel panel = new MainPanel();
+                new MainFrame(panel);
+            }
+        });
     }
 }
--- Java/GridBagConstraintsSample/main/MainPanel.java
+++ Java/GridBagConstraintsSample/main/MainPanel.java
@@ -1,56 +1,236 @@
 package main;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.MessageFormat;
+import java.util.Random;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingWorker;
 
 
+/**
+ * This class will have a main panel with BorderLayout as its default.
+ * A sub-panel will have a GridBagLayout, and this panel will be put into the
+ * main panel's NORTH location.
+ * Any sub-panels that need to expand will be placed into the main panel's
+ * CENTER location.
+ * @author Todd Nguyen
+ *
+ */
 @SuppressWarnings("serial")
 public class MainPanel extends JPanel {
+    private JProgressBar pbar;
+    private JTextArea textArea;
+    private javax.swing.JTextField jtf; 
+    private JButton btnStartProgress;
+    private CustomTask m_customTask;
+    
     public MainPanel() {
         init();
     }
     
+    /** 
+     * Make a subclass of SwingWorker.
+     * Void - the result type returned by this SwingWorker's doInBackground and get methods
+     * Integer - the type used for carrying out intermediate results by this SwingWorker's publish and process methods
+     * @author Todd Nguyen
+     *
+     */
+    private class CustomTask extends SwingWorker<Void, Integer> {
+
+        /**
+         * Main task. Execute in background
+         */
+        @Override
+        public Void doInBackground() {
+            Random random = new Random();
+            int progress = 0;
+            // Initialize progress. This is invoked using SwingWorker
+            setProgress(0);
+            while (progress < 100) {
+                // Sleep for up to one second
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                } catch (InterruptedException ignore) {};
+                
+                // Make random progress
+                progress += random.nextInt(10);
+                int tempProg = Math.min(progress,100);
+                
+                // So we don't have more than 100% completion
+                setProgress(tempProg);
+                
+                // Send to SwingWorker's process() method using publish()
+                publish(tempProg);
+            }
+            
+            return null;
+        }
+        
+        
+        @Override
+        public void process(java.util.List<Integer> integers) {
+            int lastInt = integers.get(integers.size() - 1);
+            String currentText = jtf.getText();
+            currentText += "[" + lastInt + "] ";
+            jtf.setText(currentText);
+        }
+        
+        
+        /**
+         * Executed in the Event Dispatch Thread (EDT)
+         */
+        @Override
+        public void done() {
+            Toolkit.getDefaultToolkit().beep();
+            // Reenable the button
+            btnStartProgress.setEnabled(true);
+            
+            // Reenable the cursor
+            // setCursor(null);
+            
+            textArea.append("Done!\n");
+        }
+    };
+    
     
     private void init() {
+        this.setLayout(new BorderLayout());
+        
+        JPanel pagestartPanel = this.getPagestartPanel();
+        this.add(pagestartPanel, BorderLayout.PAGE_START);
+        
+        JPanel centerPanel = this.getCenterPanel();
+        this.add(centerPanel, BorderLayout.CENTER);
+    }
+    
+    
+    private JPanel getPagestartPanel() {
+        JPanel panelPagestart = new JPanel();
         int currentYCoord = 0;
         
-        this.setLayout(new GridBagLayout());
+        panelPagestart.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         gbc.anchor = GridBagConstraints.NORTHWEST;
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.insets = new Insets(5, 0, 5, 0);
         
-        JButton button1 = new JButton("Button 1");
+        btnStartProgress = new JButton("Start Progress Bar");
+        btnStartProgress.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnStartProgress.setEnabled(false);
+                
+                // Change cursor to waiting cursor
+                // setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                
+                // Instances of javax.swing.SwingWorker are not reusuable, so
+                // we create new instances as needed.
+                m_customTask = new CustomTask();
+                m_customTask.addPropertyChangeListener(new PropertyChangeListener() {
+                    @Override
+                    public void propertyChange(PropertyChangeEvent evt) {
+                        customPropertyChange(evt);
+                    }
+                });
+                // Schedule the task on a worker thread
+                m_customTask.execute();
+            }
+        });
+
         gbc.gridy = currentYCoord++;
-        this.add(button1, gbc);
+        panelPagestart.add(btnStartProgress, gbc);
         
         JButton button2 = new JButton("Button 2 - Slightly Longer");
         gbc.gridy = currentYCoord++;
-        this.add(button2, gbc);
+        panelPagestart.add(button2, gbc);
         
         JButton button3 = new JButton("Button 3");
         gbc.gridy = currentYCoord++;
-        this.add(button3, gbc);
+        panelPagestart.add(button3, gbc);
         
         JComboBox<String> cbox = new JComboBox<> (new String[] {"5", "4", "3333"}) ;
         gbc.gridy = currentYCoord++;
-        this.add(cbox, gbc);
+        panelPagestart.add(cbox, gbc);
         
-        javax.swing.JTextField jtf = new javax.swing.JTextField();
+        jtf = new javax.swing.JTextField();
         gbc.gridy = currentYCoord++;
         GridBagConstraints gbc2 = (GridBagConstraints) gbc.clone();
         gbc2.fill = GridBagConstraints.HORIZONTAL;
-        this.add(jtf, gbc2);
+        panelPagestart.add(jtf, gbc2);
         
         JButton button4 = new JButton("Button 4");
+        button4.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "You pressed button 4!");
+            }
+        });
         gbc.gridy = currentYCoord++;
-        this.add(button4, gbc);
+        panelPagestart.add(button4, gbc);
+        
+        return panelPagestart;
+    }
+    
+    
+    private JPanel getCenterPanel() {
+        int verticalGap = 5;
+        
+        JPanel centerPanel = new JPanel();
+        
+        // Set a top margin if needed
+        centerPanel.setBorder(BorderFactory.createEmptyBorder(verticalGap, 0, 0, 0));
+        
+        // Use a BorderLayout as well to make it easy to expand
+        centerPanel.setLayout(new BorderLayout(0, verticalGap));
+        
+        // Make a progress bar with 0 as min and 100 as max
+        this.pbar = new JProgressBar(0, 100);
+        this.pbar.setValue(0);
+        this.pbar.setStringPainted(true);
+        centerPanel.add(this.pbar, BorderLayout.PAGE_START);
+        
+        // Make a text area        
+        this.textArea = new JTextArea();
+        this.textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 16));
+        this.textArea.setLineWrap(true);
+        
+        JScrollPane jsp = new JScrollPane();
+        jsp.setViewportView(this.textArea);
+        centerPanel.add(jsp, BorderLayout.CENTER);
+        
+        return centerPanel;
+    }
+    
+    
+    // Ref: https://docs.oracle.com/javase/tutorial/uiswing/components/progress.html
+    // Invoked when a property changes
+    public void customPropertyChange(java.beans.PropertyChangeEvent evt) {
+        // Check if it is progress
+        if (evt.getPropertyName().equalsIgnoreCase("progress")) {
+            int value = (Integer) evt.getNewValue();
+            this.pbar.setValue(value);
+            // Return the progress bound property
+            String s = MessageFormat.format("Completed {0}% of task\n", m_customTask.getProgress());
+            this.textArea.append(s);
+        }
     }
 }
