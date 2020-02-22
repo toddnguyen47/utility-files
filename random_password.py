5eded61809a714b7c02fd4e83597141dbbe1ea89
Todd Nguyen <todd.nguyen47@gmail.com>
2020-02-22 04:44:01 -0800

    Started to use TDD

---
+++ random_password.py
@@ -0,0 +1,35 @@
+import string
+import random
+from typing import List
+
+
+class RandomPassword:
+    def __init__(self):
+        self._special_chars_tuple = ("!", "@", "#", "$", "%")
+        self._string_desired = "".join(
+            (string.ascii_lowercase, string.digits) + self._special_chars_tuple)
+        self._base_filename = "random"
+        self._max_passwords = 200
+        self._max_files = 4
+        self._password_length = 12
+        self._index_to_replace = [i for i in range(self._password_length)]
+
+    def generate_password(self):
+        pass
+
+    def generate_random_lowercase_list(self) -> list:
+        list_of_chars = [random.choice(
+            string.ascii_lowercase) for _ in range(self._password_length)]
+        return list_of_chars
+
+    def uppercase_one_char(self, list_input: List[str]) -> list:
+        random_index = random.randrange(len(self._index_to_replace))
+        char_index_to_replace = self._index_to_replace[random_index]
+        list_input[char_index_to_replace] = list_input[char_index_to_replace].upper()
+        del self._index_to_replace[random_index]
+        return list_input
+
+
+if __name__ == "__main__":
+    random_password = RandomPassword()
+    # random_password.execute()
---
+++ random_password_test.py
@@ -0,0 +1,29 @@
+import pytest
+from random_password import RandomPassword
+
+
+@pytest.fixture(scope="module")
+def random_password():
+    random_password = RandomPassword()
+    yield random_password
+    print("Teardown now!")
+
+
+def string_is_all_lowercase(str_input: str) -> bool:
+    return all(x.islower() for x in str_input)
+
+
+def test_generate_lowercase_password(random_password):
+    s = random_password.generate_random_lowercase_list()
+    assert len(s) > 0
+    assert string_is_all_lowercase(s)
+
+def test_password_has_one_uppercase(random_password):
+    list1 = random_password.generate_random_lowercase_list()
+    list1 = random_password.uppercase_one_char(list1)
+    assert len(list1) > 0
+    assert any(x.isupper() for x in list1)
+    assert random_password._password_length - 1 == len(random_password._index_to_replace)
+
+def test_password_has_one_number(random_password):
+    pass
