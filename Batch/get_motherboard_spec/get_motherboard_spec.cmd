c69e18d4108f0de71e41b23abfaa3d4ad5097ecf
Todd Nguyen <todd.nguyen47@gmail.com>
2020-05-16 16:18:12 -0700

    Added License

--- Batch/get_motherboard_spec/get_motherboard_spec.cmd
+++ Batch/get_motherboard_spec/get_motherboard_spec.cmd
@@ -1,3 +1,4 @@
+@REM Ref: https://www.howtogeek.com/208420/how-to-check-your-motherboard-model-number-on-your-windows-pc/
 echo Remember to run this as admin!
 wmic baseboard get product,Manufacturer,version,serialnumber
 pause
