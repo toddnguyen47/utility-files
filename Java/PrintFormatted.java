c870d4f73652a514c0bc9d86e6c61ece241ca733
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-03-17 20:43:09 -0700

    Update PrintFormatted.java

--- Java/PrintFormatted.java
+++ Java/PrintFormatted.java
@@ -2,7 +2,7 @@ public class PrintFormatted {
   /**
    *  Ref: https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#format-java.lang.String-java.lang.Object...-
    */
-  private void printFmt(String format, Object... args) {
+  private void printfmtln(String format, Object... args) {
     System.out.println(String.format(format, args));
   }
 }
