468cefa61afcdab04b5081d2badf5d532e86d35d
Todd Nguyen <todd.nguyen47@gmail.com>
2021-06-25 23:06:44 -0700

    Finish `test_given_beginning_tabs_when_converting_then_return_spaces`

--- Python/convert_beginning_tabs_to_spaces/src/convert_beginning_tabs.py
+++ Python/convert_beginning_tabs_to_spaces/src/convert_beginning_tabs.py
@@ -1,6 +1,24 @@
+import re
+
+
 class ConvertBeginningTabs:
     def __init__(self) -> None:
-        pass
+        self._pattern = re.compile(r"(^\t*)([\S\s]*)")
 
-    def return_two(self) -> int:
-        return 2
+    def convert(self, input_line: str, num_spaces: int) -> str:
+        matcher = self._pattern.match(input_line)
+
+        if matcher is None:
+            return input_line
+        else:
+            tabs_str = matcher.group(1)
+            space_per_tab = self._generate_space(num_spaces)
+            space_str = tabs_str.replace("\t", space_per_tab)
+            return space_str + matcher.group(2)
+
+    def _generate_space(self, num_spaces: int) -> str:
+        list1 = []
+        space_str = ""
+        for _ in range(num_spaces):
+            list1.append(" ")
+        return space_str.join(list1)
--- Python/convert_beginning_tabs_to_spaces/tests/convert_beginning_tabs_test.py
+++ Python/convert_beginning_tabs_to_spaces/tests/convert_beginning_tabs_test.py
@@ -4,9 +4,17 @@ import pytest
 
 
 @pytest.fixture
-def convert_beginning_tabs():
+def convert_beginning_tabs() -> ConvertBeginningTabs:
     return ConvertBeginningTabs()
 
 
-def test_true(convert_beginning_tabs):
-    assert convert_beginning_tabs.return_two() == 2
+def test_given_beginning_tabs_when_converting_then_return_spaces(
+    convert_beginning_tabs: ConvertBeginningTabs,
+):
+    test_str = "		Hello World"
+    actual_str = convert_beginning_tabs.convert(test_str, 4)
+    assert "        Hello World" == actual_str
+
+
+# test no tabs at the beginning
+# test tabs within tab should not be converted
