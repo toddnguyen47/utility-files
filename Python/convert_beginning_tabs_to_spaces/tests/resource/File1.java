d3187b99411a9118daa8c48ffcd1fb71ee9867e4
Todd Nguyen <todd.nguyen47@gmail.com>
2021-06-25 23:52:36 -0700

    Add resource files to test it out on

---
+++ Python/convert_beginning_tabs_to_spaces/tests/resource/File1.java
@@ -0,0 +1,14 @@
+public class File1 {
+	public void func1() {
+		// Some code
+	}
+
+	public void func2() {
+		
+		    
+	}
+
+	public void func3WithSomeTabsInMiddle() {
+		String hello = "hello	world		!";
+	}
+}
---
+++ Python/convert_beginning_tabs_to_spaces/tests/resource/File2.java
@@ -0,0 +1,14 @@
+public class File2 {
+	public void func1() {
+		// Some code
+	}
+
+	public void func2() {
+		
+		    
+	}
+
+	public void func3WithSomeTabsInMiddle() {
+		String hello = "hello	world		!";
+	}
+}
