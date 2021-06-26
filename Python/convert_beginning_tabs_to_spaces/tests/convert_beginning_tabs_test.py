cb7b85a2d3c1510b53034dc4fa1ae3943e01fd3e
Todd Nguyen <todd.nguyen47@gmail.com>
2021-06-25 23:12:29 -0700

    Finish `test given tabs within tab when converting then inside tabs should not be converted`

--- Python/convert_beginning_tabs_to_spaces/tests/convert_beginning_tabs_test.py
+++ Python/convert_beginning_tabs_to_spaces/tests/convert_beginning_tabs_test.py
@@ -24,4 +24,9 @@ def test_given_no_tabs_at_beginning_when_converting_then_return_same_input(
     assert "Hello World" == test_str
 
 
-# test tabs within tab should not be converted
+def test_given_tabs_within_tab_when_converting_then_inside_tabs_should_not_be_converted(
+    convert_beginning_tabs: ConvertBeginningTabs,
+):
+    test_str = "		Hello 	there	World"
+    actual_str = convert_beginning_tabs.convert(test_str, 4)
+    assert "        Hello 	there	World" == actual_str
