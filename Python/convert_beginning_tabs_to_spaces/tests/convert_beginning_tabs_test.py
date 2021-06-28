c6a9da447dc037cf4ac8c52b85ec3eb2f8679c43
Todd Nguyen <todd.nguyen47@gmail.com>
2021-06-28 09:18:36 -0700

    Add test for intelligently converting tabs to 2 spaces

--- Python/convert_beginning_tabs_to_spaces/tests/convert_beginning_tabs_test.py
+++ Python/convert_beginning_tabs_to_spaces/tests/convert_beginning_tabs_test.py
@@ -21,7 +21,7 @@ def test_given_no_tabs_at_beginning_when_converting_then_return_same_input(
 ):
     test_str = "Hello World"
     actual_str = convert_beginning_tabs.convert_tabs_to_spaces(test_str, 4)
-    assert "Hello World" == test_str
+    assert actual_str == test_str
 
 
 def test_given_tabs_within_tab_when_converting_then_inside_tabs_should_not_be_converted(
@@ -52,8 +52,27 @@ def test_given_excess_whitespace_on_right_when_converting_then_remove_excess_whi
         ("	   	Hello 	there	World		   ", "        Hello 	there	World"),
     ],
 )
-def test_given_one_space_then_tabs_combination_when_converting_with_4_spaces_per_tab_then_convert_correctly(
+def test_given_space_tabs_combination_when_converting_with_4_spaces_per_tab_then_convert_correctly(
     convert_beginning_tabs: ConvertBeginningTabs, input: str, expected: str
 ):
     actual_str = convert_beginning_tabs.convert_tabs_to_spaces(input, 4)
+    assert expected == actual_str
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("        Hello 	there	World		   ", "        Hello 	there	World"),
+        (" 		Hello 	there	World		   ", "    Hello 	there	World"),
+        ("  		Hello 	there	World		   ", "      Hello 	there	World"),
+        ("   		Hello 	there	World		   ", "      Hello 	there	World"),
+        ("	 	Hello 	there	World		   ", "    Hello 	there	World"),
+        ("	  	Hello 	there	World		   ", "      Hello 	there	World"),
+        ("	   	Hello 	there	World		   ", "      Hello 	there	World"),
+    ],
+)
+def test_given_space_tabs_combination_when_converting_with_2_spaces_per_tab_then_convert_correctly(
+    convert_beginning_tabs: ConvertBeginningTabs, input: str, expected: str
+):
+    actual_str = convert_beginning_tabs.convert_tabs_to_spaces(input, 2)
     assert expected == actual_str
