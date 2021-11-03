0153d1be30c72ea8c6c7c3e8bc711881c2fef415
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-11-03 15:58:42 -0700

    Create GetTimeInUtc.java

---
+++ Java/GetTimeInUtc/GetTimeInUtc.java
@@ -0,0 +1,21 @@
+package testutils;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public final class GetTimeInUtc {
+    private GetTimeInUtc() {
+    }
+
+    /**
+     * Sample output: 2021-11-03T22:52:57Z
+     */
+    public static String getTimeInUtc() {
+        final OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
+        final String pattern = "yyyy-MM-dd'T'HH:mm:ssX";
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+        final String timeString = now.format(dateTimeFormatter);
+        return timeString;
+    }
+}
