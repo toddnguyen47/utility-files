eba2499680e22fb1fe278ff2fd09a0133bddc51d
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-11-03 16:36:51 -0700

    Update GetTimeInUtc.java

--- Java/GetTimeInUtc/GetTimeInUtc.java
+++ Java/GetTimeInUtc/GetTimeInUtc.java
@@ -1,22 +1,23 @@
-package testutils;
+package com.util;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public final class GetTimeInUtc {
+    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ssX";
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+
     private GetTimeInUtc() {
     }
 
     /**
-     * Sample output: 2021-11-03T22:52:57Z
-     * Reference: https://stackoverflow.com/a/19632076/6323360
+     * Sample output: 2021-11-03T22:52:57Z Reference:
+     * https://stackoverflow.com/a/19632076/6323360
      */
     public static String getCurrentTimeInUtc() {
         final OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
-        final String pattern = "yyyy-MM-dd'T'HH:mm:ssX";
-        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
-        final String timeString = now.format(dateTimeFormatter);
+        final String timeString = now.format(DATE_TIME_FORMATTER);
         return timeString;
     }
 }
