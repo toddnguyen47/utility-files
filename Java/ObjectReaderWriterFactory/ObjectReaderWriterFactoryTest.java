e9e84f3ebe181311f569ec5473b2e71615926d40
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-11-16 09:36:31 -0800

    Update ObjectReaderWriterFactoryTest.java

--- Java/ObjectReaderWriterFactory/ObjectReaderWriterFactoryTest.java
+++ Java/ObjectReaderWriterFactory/ObjectReaderWriterFactoryTest.java
@@ -1,17 +1,33 @@
 package com.util;
 
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class ObjectReaderWriterFactoryTest {
     @Test
-    public void test_GivenObjectReaderCreated_WhenCallingFactoryMethod_ThenInstanceIsOfObjectReaderClass() {
+    public void test_GivenObjectReaderCreated_WhenCallingFactoryMethod_ThenInstanceIsOfObjectReaderClass()
+            throws Exception {
         final ObjectReader objectReader = ObjectReaderWriterFactory.createObjectReader();
 
+        final JsonNode jsonNode = objectReader.readValue("{}");
+
         Assert.assertEquals("class com.fasterxml.jackson.databind.ObjectReader", objectReader.getClass().toString());
+        Assert.assertNotNull(jsonNode);
+    }
+
+    @Test
+    public void test_GivenObjectReaderCreatedWithClass_WhenCallingFactoryMethod_ThenInstanceIsOfObjectReaderClass()
+            throws Exception {
+        final ObjectReader objectReader = ObjectReaderWriterFactory.createObjectReader(Map.class);
+
+        final Map<String, Object> jsonNode = objectReader.readValue("{}");
+
+        Assert.assertEquals("class com.fasterxml.jackson.databind.ObjectReader", objectReader.getClass().toString());
+        Assert.assertNotNull(jsonNode);
     }
 
     @Test
