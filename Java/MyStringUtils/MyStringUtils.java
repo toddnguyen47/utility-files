90fdf3cb46659cf47814cae34119b11544baebf6
Todd Nguyen <todd.nguyen47@gmail.com>
2022-02-03 08:35:03 -0800

    Use 500 for `DEFAULT_MAX_WIDTH`

--- Java/MyStringUtils/MyStringUtils.java
+++ Java/MyStringUtils/MyStringUtils.java
@@ -2,29 +2,29 @@ package mystringutils;
 
 public final class MyStringUtils {
 
-	private static final int DEFAULT_MAX_WIDTH = 100;
+    private static final int DEFAULT_MAX_WIDTH = 500;
 
-	private MyStringUtils() {
-	}
+    private MyStringUtils() {
+    }
 
-	/**
-	 * <div>Remove all whitespace, then return abbreviated string</div>
-	 *
-	 * <div>Ref: https://stackoverflow.com/a/5455809/6323360</div>
-	 *
-	 * @param str
-	 * @return
-	 */
-	public static String getAbbrevStringNoWhitespace(final String str) {
-		return getAbbrevStringNoWhitespace(str, DEFAULT_MAX_WIDTH);
-	}
+    /**
+     * <div>Remove all whitespace, then return abbreviated string</div>
+     *
+     * <div>Ref: https://stackoverflow.com/a/5455809/6323360</div>
+     *
+     * @param str
+     * @return
+     */
+    public static String getAbbrevStringNoWhitespace(final String str) {
+        return getAbbrevStringNoWhitespace(str, DEFAULT_MAX_WIDTH);
+    }
 
-	public static String getAbbrevStringNoWhitespace(final String str, final int maxWidth) {
-		// GUARD
-		if (StringUtils.isBlank(str)) {
-			return str;
-		}
-		final String strNoWhitespace = str.replaceAll("\\s", "");
-		return StringUtils.abbreviate(strNoWhitespace, maxWidth);
-	}
+    public static String getAbbrevStringNoWhitespace(final String str, final int maxWidth) {
+        // GUARD
+        if (StringUtils.isBlank(str)) {
+            return str;
+        }
+        final String strNoWhitespace = str.replaceAll("\\s", "");
+        return StringUtils.abbreviate(strNoWhitespace, maxWidth);
+    }
 }
