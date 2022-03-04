314697f890ae66024a3bb835b7129092d8d58564
Todd Nguyen <todd.nguyen47@gmail.com>
2022-03-04 15:03:32 -0800

    Rename folder from `ObjectMapperHelper` to `ObjectMapperConfig`

--- Java/ObjectMapperConfig/ObjectMapperHelper.java
+++ Java/ObjectMapperConfig/ObjectMapperHelper.java
@@ -12,26 +12,26 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
  * some functions of `ObjectMapper`. This is because according to `fasterxml`'s documentation,
  * `ObjectMapper`s are often very heavy; thus, lightweight instances of `ObjectReader` and
  * `ObjectWriter` should be used instead. </div>
- * 
+ *
  * <div> Code Examples:
- * 
+ *
  * <pre>
  * // To read a value
  * final ObjectReader objectReader = ObjectMapperHelper.createObjectReader();
  * final JsonNode jsonNode = objectReader.readValue({inputString});
- * 
+ *
  * // To read into a List<String>
  * final ObjectReader objectReaderList = ObjectMapperHelper.createObjectReader(new TypeReference<List<String>>(){});
  * final List<String> list1 = objectReaderList.readValue({inputString});
- * 
+ *
  * // To write a POJO as a String
  * final ObjectWriter objectWriter = ObjectMapperHelper.createObjectWriter();
  * final String str1 = objectWriter.writeValueAsString({inputJsonSerializableObject});
- * 
+ *
  * // To convert object to JsonNode
  * final JsonNode jsonNode = ObjectMapperHelper.convertObjectToJsonNode({inputObject});
  * </pre>
- * 
+ *
  * </div>
  */
 public final class ObjectMapperHelper {
