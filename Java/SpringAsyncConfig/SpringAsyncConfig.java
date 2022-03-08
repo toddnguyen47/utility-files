28317d1baa8b40302e9231a01de9804554187e16
Todd Nguyen <todd.nguyen47@gmail.com>
2022-03-07 16:23:35 -0800

    Add setting of `core.pool.size`

--- Java/SpringAsyncConfig/SpringAsyncConfig.java
+++ Java/SpringAsyncConfig/SpringAsyncConfig.java
@@ -1,6 +1,7 @@
 package springasyncconfig;
 
 import java.util.concurrent.Executor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,9 +16,18 @@ public class SpringAsyncConfig {
 
     public static final String THREAD_POOL_NAME = "myThreadPoolTaskExecutor";
 
+    private final int corePoolSize;
+
+    public SpringAsyncConfig(
+        @Value("${core.pool.size}") final int corePoolSize
+    ) {
+        this.corePoolSize = corePoolSize;
+    }
+
     @Bean(name = THREAD_POOL_NAME)
     public Executor threadPoolTaskExecutor() {
         final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(corePoolSize);
         return threadPoolTaskExecutor;
     }
 }
--- Java/SpringAsyncConfig/SpringAsyncConfigTest.java
+++ Java/SpringAsyncConfig/SpringAsyncConfigTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 
 public class SpringAsyncConfigTest {
 
+    private static final int CORE_POOL_SIZE = 4;
+
     @Test
     public void test_CreationOfThreadPoolExecutorsIsCorrect() {
-        final SpringAsyncConfig springAsyncConfig = new SpringAsyncConfig();
+        final SpringAsyncConfig springAsyncConfig = new SpringAsyncConfig(CORE_POOL_SIZE);
 
         final Executor executor = springAsyncConfig.threadPoolTaskExecutor();
 
