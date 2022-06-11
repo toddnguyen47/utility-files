87c5f13fe187b882d1e0c970795523d64cc255be
Todd Nguyen <todd.nguyen47@gmail.com>
2022-06-11 10:10:09 -0700

    Not working: using html package

--- Python/split_h1_headers/internal/splith1headers/split1headers.go
+++ Python/split_h1_headers/internal/splith1headers/split1headers.go
@@ -9,15 +9,23 @@ import (
 	"golang.org/x/net/html"
 )
 
+var constants = struct {
+	baseFileName string
+}{
+	baseFileName: "body",
+}
+
 type nodesStruct struct {
-	Nodes    [][]*html.Node
-	curIndex int
+	Nodes        [][]*html.Node
+	curIndex     int
+	notesSection bool
 }
 
 func NewNodesStruct() nodesStruct {
 	return nodesStruct{
-		Nodes:    make([][]*html.Node, 0),
-		curIndex: -1,
+		Nodes:        make([][]*html.Node, 1),
+		curIndex:     0,
+		notesSection: false,
 	}
 }
 
@@ -35,17 +43,14 @@ func (n *nodesStruct) Split(inputFile string) {
 	}
 
 	n.parseNode(rootNode)
-	fmt.Println(len(n.Nodes))
+	n.renderNodes()
 }
 
 func (n *nodesStruct) parseNode(node *html.Node) {
-	if node.Type == html.ElementNode {
+	if node.Type == html.ElementNode && !isNodeSkipped(node) {
 		if node.Data == "h1" {
 			n.curIndex += 1
 			n.Nodes = append(n.Nodes, make([]*html.Node, 0))
-			n.Nodes[n.curIndex] = append(n.Nodes[n.curIndex], node)
-		} else if node.Data == "p" {
-			// Simply append
 			n.Nodes[n.curIndex] = append(n.Nodes[n.curIndex], node)
 		} else if node.Data == "image" {
 			src := ""
@@ -66,12 +71,55 @@ func (n *nodesStruct) parseNode(node *html.Node) {
 					Data: "img",
 				}
 				n.Nodes[n.curIndex] = append(n.Nodes[n.curIndex], newNode)
-				fmt.Printf("%v\n", newNode)
-				panic("")
 			}
+		} else {
+			// Simply append
+			n.Nodes[n.curIndex] = append(n.Nodes[n.curIndex], node)
 		}
 	}
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
 		n.parseNode(child)
 	}
+}
+
+func (n *nodesStruct) renderNodes() {
+	for i, nodesPerFile := range n.Nodes {
+		// TODO: CHECK FOR NOTES
+		fileName := fmt.Sprintf("%s%02d.xhtml", constants.baseFileName, i)
+		rootNode := html.Node{
+			Type: html.ElementNode,
+			Data: "div",
+		}
+
+		for _, node := range nodesPerFile {
+			rootNode.AppendChild(node)
+		}
+
+		fmt.Printf("Writing to file: %s\n", fileName)
+		var builder strings.Builder
+		html.Render(&builder, &rootNode)
+		err := os.WriteFile(fileName, []byte(builder.String()), 0644)
+		if err != nil {
+			fmt.Printf("ERROR WRITING TO FILE %s\n", fileName)
+		}
+	}
+}
+
+func isNodeSkipped(node *html.Node) bool {
+	// Skip svg
+	if node.Data == "svg" {
+		return true
+	}
+
+	// Skip <div class="svg_outer"
+	if node.Data == "div" {
+		attributes := node.Attr
+		for _, attr := range attributes {
+			if attr.Key == "class" && strings.Contains(attr.Val, "svg_outer") {
+				return true
+			}
+		}
+	}
+
+	return false
 }
