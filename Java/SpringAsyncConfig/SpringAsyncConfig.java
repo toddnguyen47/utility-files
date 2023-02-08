d3ac06c9084e74525cb0ad11d877d62201604c96
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2023-02-08 09:11:08 -0800

    Update SpringAsyncConfig.java

--- Java/SpringAsyncConfig/SpringAsyncConfig.java
+++ Java/SpringAsyncConfig/SpringAsyncConfig.java
@@ -21,21 +21,22 @@ import java.util.concurrent.Executor;
 @EnableAsync
 public class SpringAsyncConfig {
 
-    public static final String THREAD_POOL_NAME = "myThreadPoolTaskExecutor";
+  public static final String THREAD_POOL_NAME = "myThreadPoolTaskExecutor";
 
-    private final int threadCorePoolSize;
+  private final int threadCorePoolSize;
 
-    public SpringAsyncConfig(@Value("${thread.core.pool.size}") final int corePoolSize) {
-        this.threadCorePoolSize = corePoolSize;
-    }
+  public SpringAsyncConfig(@Value("${thread.core.pool.size}") final int corePoolSize) {
+    this.threadCorePoolSize = corePoolSize;
+  }
 
-    @Bean(name = THREAD_POOL_NAME)
-    public Executor threadPoolTaskExecutor() {
-        final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        // Create a fixed size pool by setting core pool size and max pool size
-        // Ref: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ThreadPoolExecutor.html
-        threadPoolTaskExecutor.setCorePoolSize(threadCorePoolSize);
-        threadPoolTaskExecutor.setMaxPoolSize(threadCorePoolSize);
-        return threadPoolTaskExecutor;
-    }
+  @Bean(name = THREAD_POOL_NAME)
+  public Executor threadPoolTaskExecutor() {
+    final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+    // Create a fixed size pool by setting core pool size and max pool size
+    // Ref: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ThreadPoolExecutor.html
+    threadPoolTaskExecutor.setCorePoolSize(threadCorePoolSize);
+    threadPoolTaskExecutor.setMaxPoolSize(threadCorePoolSize);
+    threadPoolTaskExecutor.initialize();
+    return threadPoolTaskExecutor;
+  }
 }
