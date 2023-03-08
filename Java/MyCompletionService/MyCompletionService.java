1a92089932c041093b800bd0c1d8bb01890a455d
Todd Nguyen <todd.nguyen47@gmail.com>
2023-03-08 10:14:09 -0800

    Converting spaces in `MyCompletionService`

--- Java/MyCompletionService/MyCompletionService.java
+++ Java/MyCompletionService/MyCompletionService.java
@@ -13,38 +13,38 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
-* Sample usage of `CompletionService`.
+* Sample usage of {@code CompletionService}.
 *
 * Ref: https://stackoverflow.com/a/11872604/6323360
 */
 public final class MyCompletionService {
-	private static final Logger LOGGER = LogManager.getLogger(MyCompletionService.class);
+  private static final Logger LOGGER = LogManager.getLogger(MyCompletionService.class);
 
-	public static <T> void executeMultithreaded(
-			final int numThreads, final List<Callable<T>> callables) throws IOException {
-		// Per channel UUID
-		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-		CompletionService<T> completionService = new ExecutorCompletionService<>(executor);
+  public static <T> void executeMultithreaded(
+      final int numThreads, final List<Callable<T>> callables) throws IOException {
+    // Per channel UUID
+    ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+    CompletionService<T> completionService = new ExecutorCompletionService<>(executor);
 
-		// Submit tasks
-		for (int i = 0; i < callables.size(); i++) {
-			Callable<T> callable = callables.get(i);
-			completionService.submit(callable);
-		}
-		executor.shutdown();
+    // Submit tasks
+    for (int i = 0; i < callables.size(); i++) {
+      Callable<T> callable = callables.get(i);
+      completionService.submit(callable);
+    }
+    executor.shutdown();
 
-		// Wait for tasks to complete
-		try {
-			while (!executor.isTerminated()) {
-				Future<T> future = completionService.take();
-				T results = future.get();
-				LOGGER.debug(results);
-			}
-		} catch (final InterruptedException | ExecutionException e) {
-			for (final StackTraceElement elem : e.getStackTrace()) {
-				LOGGER.error(elem);
-			}
-		}
-		LOGGER.info("multithreaded execution finished.");
-	}
+    // Wait for tasks to complete
+    try {
+      while (!executor.isTerminated()) {
+        Future<T> future = completionService.take();
+        T results = future.get();
+        LOGGER.debug(results);
+      }
+    } catch (final InterruptedException | ExecutionException e) {
+      for (final StackTraceElement elem : e.getStackTrace()) {
+        LOGGER.error(elem);
+      }
+    }
+    LOGGER.info("multithreaded execution finished.");
+  }
 }
