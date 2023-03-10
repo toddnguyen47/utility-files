a214de6d6e6577afcd66be812aa0835cee3b4517
Todd Nguyen <todd.nguyen47@gmail.com>
2023-03-09 18:46:07 -0800

    Add `invokeAll()`

--- Java/MyCompletionService/MyCompletionService.java
+++ Java/MyCompletionService/MyCompletionService.java
@@ -1,6 +1,7 @@
 package MyCompletionService;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
@@ -13,22 +14,31 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
-* Sample usage of {@code CompletionService}.
+* Sample usage of {@code CompletionService} and various other multithreaded functions.
 *
 * Ref: https://stackoverflow.com/a/11872604/6323360
 */
 public final class MyCompletionService {
   private static final Logger LOGGER = LogManager.getLogger(MyCompletionService.class);
 
-  public static <T> void executeMultithreaded(
+  private MyCompletionService() {}
+
+  /**
+  * Use `CompletionService` to invoke multithread actions
+  * @param <T>
+  * @param numThreads
+  * @param callables
+  * @throws IOException
+  */
+  public static <T> void executeWithCompletionService(
       final int numThreads, final List<Callable<T>> callables) throws IOException {
-    // Per channel UUID
     ExecutorService executor = Executors.newFixedThreadPool(numThreads);
     CompletionService<T> completionService = new ExecutorCompletionService<>(executor);
 
     // Submit tasks
-    for (int i = 0; i < callables.size(); i++) {
-      Callable<T> callable = callables.get(i);
+    Iterator<Callable<T>> iter = callables.iterator();
+    while (iter.hasNext()) {
+      Callable<T> callable = iter.next();
       completionService.submit(callable);
     }
     executor.shutdown();
@@ -45,6 +55,30 @@ public final class MyCompletionService {
         LOGGER.error(elem);
       }
     }
-    LOGGER.info("multithreaded execution finished.");
+    LOGGER.info("executeWithCompletionService() execution finished.");
+  }
+
+  /**
+  * Use `invokeAll()` to invoke multithread actions
+  * @param <T>
+  * @param numThreads
+  * @param callables
+  * @throws IOException
+  */
+  public static <T> void invokeAll(final int numThreads, final List<Callable<T>> callables)
+      throws IOException {
+
+    ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+    try {
+      List<Future<T>> results = executor.invokeAll(callables);
+      LOGGER.debug("results len: {}", results.size());
+    } catch (final InterruptedException e) {
+      for (final StackTraceElement elem : e.getStackTrace()) {
+        LOGGER.error(elem);
+      }
+      throw new IOException(e);
+    }
+    executor.shutdown();
+    LOGGER.info("invokeAll() execution finished.");
   }
 }
