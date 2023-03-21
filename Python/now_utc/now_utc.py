d52afc3b61d4e978b6701349158ecd6db33b6511
Todd Nguyen <todd.nguyen47@gmail.com>
2023-03-21 12:33:05 -0700

    Add some minutes checking

--- Python/now_utc/now_utc.py
+++ Python/now_utc/now_utc.py
@@ -2,21 +2,34 @@
 
 from datetime import datetime, timedelta
 
+_FILL_SPACES = 10
+
 
 def _main():
     """Main function"""
     now = datetime.utcnow()
-    start = datetime(now.year, now.month, now.day, now.hour)
+    minutes = _get_minute(now)
+    start = datetime(now.year, now.month, now.day, now.hour, minutes)
     end = start + timedelta(minutes=30)
 
-    print(f"now: {_get_timeformat(now)}")
-    print(f"start: {_get_timeformat(start)}")
-    print(f"end: {_get_timeformat(end)}")
+    print(f'{"now:": <{_FILL_SPACES}}{_get_timeformat(now)}')
+    print(f'{"start:": <{_FILL_SPACES}}{_get_timeformat(start)}')
+    print(f'{"end:" : <{_FILL_SPACES}}{_get_timeformat(end)}')
 
 
 def _get_timeformat(input_time: datetime) -> str:
     """Get time format"""
     return input_time.isoformat(timespec="milliseconds") + "Z"
+
+
+def _get_minute(input_time: datetime) -> int:
+    """Get minute and round down to nearest half hour"""
+    minutes = input_time.minute
+    if minutes > 30:
+        minutes = 30
+    else:
+        minutes = 0
+    return minutes
 
 
 if __name__ == "__main__":
