722c32532db990d701e10f97dafcfda164e35d01
Todd Nguyen <todd.nguyen47@gmail.com>
2023-04-17 14:34:40 -0700

    Use 4 spaces instead of 2 spaces in `SpringAsyncConfig`

--- Java/SpringAsyncConfig/SpringAsyncConfig.java
+++ Java/SpringAsyncConfig/SpringAsyncConfig.java
@@ -13,30 +13,30 @@ import java.util.concurrent.Executor;
  *
  * Usage:
  * <pre>
- *  @Async(value = SpringAsyncConfig.THREAD_POOL_NAME)
- *  public void functionName() {}
+ *    @Async(value = SpringAsyncConfig.THREAD_POOL_NAME)
+ *    public void functionName() {}
  * </pre>
  */
 @Configuration
 @EnableAsync
 public class SpringAsyncConfig {
 
-  public static final String THREAD_POOL_NAME = "myThreadPoolTaskExecutor";
+    public static final String THREAD_POOL_NAME = "myThreadPoolTaskExecutor";
 
-  private final int threadCorePoolSize;
+    private final int threadCorePoolSize;
 
-  public SpringAsyncConfig(@Value("${thread.core.pool.size}") final int corePoolSize) {
-    this.threadCorePoolSize = corePoolSize;
-  }
+    public SpringAsyncConfig(@Value("${thread.core.pool.size}") final int corePoolSize) {
+        this.threadCorePoolSize = corePoolSize;
+    }
 
-  @Bean(name = THREAD_POOL_NAME)
-  public Executor threadPoolTaskExecutor() {
-    final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-    // Create a fixed size pool by setting core pool size and max pool size
-    // Ref: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ThreadPoolExecutor.html
-    threadPoolTaskExecutor.setCorePoolSize(threadCorePoolSize);
-    threadPoolTaskExecutor.setMaxPoolSize(threadCorePoolSize);
-    threadPoolTaskExecutor.initialize();
-    return threadPoolTaskExecutor;
-  }
+    @Bean(name = THREAD_POOL_NAME)
+    public Executor threadPoolTaskExecutor() {
+        final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        // Create a fixed size pool by setting core pool size and max pool size
+        // Ref: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ThreadPoolExecutor.html
+        threadPoolTaskExecutor.setCorePoolSize(threadCorePoolSize);
+        threadPoolTaskExecutor.setMaxPoolSize(threadCorePoolSize);
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
 }
