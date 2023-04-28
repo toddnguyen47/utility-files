84fbde357ed6d02991add50d2944098dd9c25262
Todd Nguyen <todd.nguyen47@gmail.com>
2023-04-28 11:40:27 -0700

    Add `aws sts`

---
+++ Java/src/main/java/com/github/toddnguyen47/awssts/AwsSts.java
@@ -0,0 +1,28 @@
+package com.github.toddnguyen47.awssts;
+
+import com.amazonaws.auth.STSSessionCredentialsProvider;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+
+public class AwsSts {
+    public AmazonSimpleEmailService initializeSimpleEmailServiceClient(String awsRegion) {
+
+        AWSSecurityTokenService stsService =
+                AWSSecurityTokenServiceClientBuilder.standard().withRegion(awsRegion).build();
+        STSSessionCredentialsProvider credentialsProviderAutoRefresh =
+                new STSSessionCredentialsProvider(stsService);
+        AmazonSimpleEmailService client =
+                AmazonSimpleEmailServiceClientBuilder.standard()
+                        .withCredentials(credentialsProviderAutoRefresh)
+                        .withRegion(awsRegion)
+                        .build();
+
+        if (client == null) {
+            // TODO: Log error here!
+        }
+
+        return client;
+    }
+}
