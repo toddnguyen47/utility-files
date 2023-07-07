17346cfc71b7a570dbcd5a0493ff634510fdb454
Todd Nguyen <todd.nguyen47@gmail.com>
2023-07-07 12:17:32 -0700

    Add `RetryJitter`

---
+++ Java/src/main/java/com/github/toddnguyen47/retryjitter/RetryJitter.java
@@ -0,0 +1,63 @@
+package com.github.toddnguyen47.retryjitter;
+
+import java.time.Instant;
+import java.util.Random;
+
+/**
+ * RetryJitter - Ref: https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+ */
+public final class RetryJitter {
+
+    private static final Instant NOW = Instant.now();
+    private static final Random RANDOM = new Random(NOW.toEpochMilli());
+    private static final int MIN_SLEEP_TIME_MILLIS = 50;
+
+    private RetryJitter() {}
+
+    public static void retry(
+            final int retryTimes, final int timeoutMilliseconds, final RetryFunction retryFunction)
+            throws RetryJitterException {
+        boolean keepRetrying = true;
+        int timeoutMillisInner = timeoutMilliseconds;
+        if (timeoutMillisInner < 0) {
+            // Defaults to 100 milliseconds
+            timeoutMillisInner = 100;
+        }
+        int count = 0;
+
+        for (; count <= retryTimes && keepRetrying; count++) {
+            if (count > 0) {
+                int maxSleep = (timeoutMillisInner << (count - 1)) + 1 - MIN_SLEEP_TIME_MILLIS;
+                int randomSleepTime = RANDOM.nextInt(maxSleep) + MIN_SLEEP_TIME_MILLIS;
+                // TODO: LOG HERE
+                System.out.printf(
+                        "Current retry count: %d, Sleep for: %d millis\n", count, randomSleepTime);
+                sleepFor(randomSleepTime);
+            }
+            boolean results = retryFunction.run();
+            if (results) {
+                keepRetrying = false;
+            }
+        }
+
+        // Failure - max retry count reached
+        if (keepRetrying) {
+            String msg =
+                    String.format("retry count '%d' exceeds max retry count of '%d'", count, retryTimes);
+            throw new RetryJitterException(msg);
+        }
+    }
+
+    private static void sleepFor(final long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @FunctionalInterface
+    public interface RetryFunction {
+        boolean run();
+    }
+}
---
+++ Java/src/test/java/com/github/toddnguyen47/retryjitter/RetryJitterTest.java
@@ -0,0 +1,70 @@
+package com.github.toddnguyen47.retryjitter;
+
+import com.github.toddnguyen47.retryjitter.RetryJitter.RetryFunction;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RetryJitterTest {
+
+    @Test
+    public void Test_GivenRetrySuccess_ThenNoExceptionIsThrown() {
+        // -- ARRANGE --
+        RetryFunctionTest rft = new RetryFunctionTest();
+        // -- ACT --
+        // -- ASSERT --
+        try {
+            RetryJitter.retry(3, 100, rft);
+        } catch (final RetryJitterException e) {
+            Assert.fail("no exceptions should have been thrown");
+        }
+    }
+
+    @Test
+    public void Test_GivenRetryFailureAtFirstButThenSucecss_ThenNoExceptionIsThrown() {
+        // -- ARRANGE --
+        RetryFunctionTest rft = new RetryFunctionTest();
+        rft.code = "FFFP";
+        // -- ACT --
+        // -- ASSERT --
+        try {
+            RetryJitter.retry(3, -1, rft);
+        } catch (final RetryJitterException e) {
+            Assert.fail("no exceptions should have been thrown");
+        }
+    }
+
+    @Test
+    public void Test_GivenRetryFailure_ThenThrowException() {
+        // -- ARRANGE --
+        RetryFunctionTest rft = new RetryFunctionTest();
+        rft.code = "FFFFFF";
+        // -- ACT --
+        // -- ASSERT --
+        try {
+            RetryJitter.retry(3, -1, rft);
+            Assert.fail("exceptions should have been thrown");
+        } catch (final RetryJitterException e) {
+            // We pass! Do nothing
+        }
+    }
+
+    private class RetryFunctionTest implements RetryFunction {
+
+        /*
+         * code - "FFFPPP", "F" for fail, "P" for pass
+         */
+        public String code = "";
+
+        @Override
+        public boolean run() {
+            if (!code.isEmpty()) {
+                char firstChar = code.charAt(0);
+                code = code.substring(1);
+                if (firstChar == 'F') {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
