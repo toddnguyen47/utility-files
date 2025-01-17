d269766c55a2160a1cd12d03b22e46a0192492bf
Todd Nguyen-Tran <todd.nguyen47@gmail.com>
2025-01-17 00:55:37 -0800

    Add longer passwords and randomly generate [min, max] of special characters and numbers

--- python/random_password/random_password.py
+++ python/random_password/random_password.py
@@ -1,6 +1,6 @@
 import string
-import random
 from typing import List
+import secrets
 
 
 class RandomPassword:
@@ -14,20 +14,24 @@ class RandomPassword:
 
     class DigitReplacement(IReplacement):
         def replace(self, str_input: str) -> str:
-            return str(random.randrange(9 + 1))
+            return str(secrets.randbelow(9 + 1))
 
     class SpecialCharsReplacement(IReplacement):
         def __init__(self):
             self._special_chars_tuple = ("!", "@", "#", "$", "%")
 
         def replace(self, str_input: str) -> str:
-            return random.choice(self._special_chars_tuple)
+            return secrets.choice(self._special_chars_tuple)
 
     def __init__(self):
         self._base_filename = "random"
         self._max_passwords = 200
         self._max_files = 4
-        self._password_length = 12
+        self._password_length = 16
+        self._max_special_chars = 3
+        self._min_special_chars = 2
+        self._max_amount_of_numbers = 3
+        self._min_amount_of_numbers = 2
         self._static_index_to_replace = [i for i in range(self._password_length)]
         self._index_to_replace: List = []
         self.current_random_password_list: List = []
@@ -39,25 +43,26 @@ class RandomPassword:
         print("Finished!")
 
     def output_passwords_to_file(self, filename: str):
-      with open(filename, "w") as text_file:
-          for j in range(0, self._max_passwords):
-              temp_str = "{:03d}. {}\n".format(j + 1, self.generate_password())
-              text_file.write(temp_str)
+        with open(filename, "w") as text_file:
+            for j in range(0, self._max_passwords):
+                temp_str = "{:03d}. {}\n".format(j + 1, self.generate_password())
+                text_file.write(temp_str)
 
     def generate_password(self) -> str:
         self.reset_index_to_replace()
         self.set_current_random_password_all_lowercase()
-        self.replace_five_lowercase_chars()
+        self.replace_some_lowercase_chars()
         return "".join(self.current_random_password_list)
 
     def set_current_random_password_all_lowercase(self):
-        self.current_random_password_list = [random.choice(
-            string.ascii_lowercase) for _ in range(self._password_length)]
+        self.current_random_password_list = [
+            secrets.choice(string.ascii_lowercase) for _ in range(self._password_length)
+        ]
 
-    def replace_five_lowercase_chars(self):
+    def replace_some_lowercase_chars(self):
         self.uppercase_one_char()
-        self.replace_two_chars_with_number()
-        self.replace_two_chars_with_special_chars()
+        self.replace_chars_with_number()
+        self.replace_chars_with_special_chars()
 
     def reset_index_to_replace(self):
         self._index_to_replace = self._static_index_to_replace.copy()
@@ -65,18 +70,25 @@ class RandomPassword:
     def uppercase_one_char(self):
         self._generic_replace(self.UpperReplacement())
 
-    def replace_two_chars_with_number(self):
-        self._generic_replace(self.DigitReplacement(), n_times=2)
+    def replace_chars_with_number(self):
+        n_times = secrets.choice(
+            [self._min_amount_of_numbers, self._max_amount_of_numbers]
+        )
+        self._generic_replace(self.DigitReplacement(), n_times=n_times)
 
-    def replace_two_chars_with_special_chars(self):
-        self._generic_replace(self.SpecialCharsReplacement(), n_times=2)
+    def replace_chars_with_special_chars(self):
+        n_times = secrets.choice([self._min_special_chars, self._max_special_chars])
+        self._generic_replace(self.SpecialCharsReplacement(), n_times=n_times)
 
     def _generic_replace(self, replacement_object: IReplacement, n_times: int = 1):
         for _ in range(n_times):
-            random_index = random.randrange(len(self._index_to_replace))
+            random_index = secrets.randbelow(len(self._index_to_replace))
             char_index_to_replace = self._index_to_replace[random_index]
-            self.current_random_password_list[char_index_to_replace] = replacement_object.replace(
-                self.current_random_password_list[char_index_to_replace])
+            self.current_random_password_list[char_index_to_replace] = (
+                replacement_object.replace(
+                    self.current_random_password_list[char_index_to_replace]
+                )
+            )
             del self._index_to_replace[random_index]
 
 
